e104c2cb5d44f329dd2c6da49841a50435612b65
Jo Wagner <jowagner@limago.de>
2022-07-04 12:23:58 +0200

    Initial commit

---
+++ simple-console-app/src/main/java/de/db/simpleconsoleapp/SimpleConsoleAppApplication.java
@@ -0,0 +1,13 @@
+package de.db.simpleconsoleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleConsoleAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleConsoleAppApplication.class, args);
+    }
+
+}
---
+++ simple-console-app/src/test/java/de/db/simpleconsoleapp/SimpleConsoleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.db.simpleconsoleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleConsoleAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
